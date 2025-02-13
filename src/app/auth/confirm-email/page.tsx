a5cd56e1fce1c27ca28c663d1c24eaa169efcf79
VS <vvs.jkr@gmail.com>
2025-02-13 15:38:37 +0300

    fix bug

--- src/app/auth/confirm-email/page.tsx
+++ src/app/auth/confirm-email/page.tsx
@@ -1,38 +1,49 @@
-'use client'; 
+'use client'
 
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation'; 
-import { verificationService } from '@/features/auth/services'; // Обновите путь
-import { toast } from 'sonner';
+import { useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
+import { toast } from 'sonner'
 
-const ConfirmEmailPage = () => { 
-  const router = useRouter();
-  const searchParams = new URLSearchParams(window.location.search); 
-  const token = searchParams.get('token'); 
+import { verificationService } from '@/features/auth/services'
 
-  useEffect(() => {
-    if (token && typeof token === 'string') {
-      const confirmEmail = async () => {
-        try {
-          await verificationService.newVerification(token);
-          toast.success('Email успешно подтвержден!');
-          router.push('/auth/login');
-        } catch (error: any) {
-          toast.error(`Ошибка подтверждения email: ${error.response?.data?.message || 'Неизвестная ошибка'}`);
-          router.push('/auth/register');
-        }
-      };
+const ConfirmEmailPage = () => {
+	const router = useRouter()
+	const [token, setToken] = useState<string | null>(null)
 
-      confirmEmail();
-    }
-  }, [token, router]);
+	useEffect(() => {
+		const searchParams = new URLSearchParams(window.location.search)
+		const tokenFromParams = searchParams.get('token')
 
-  return (
-    <div>
-      <h1>Подтверждение email...</h1>
-      <p>Пожалуйста, подождите...</p>
-    </div>
-  );
-};
+		if (!tokenFromParams) {
+			toast.error('Неверная ссылка подтверждения.')
+			router.push('/auth/register')
+			return
+		}
 
-export default ConfirmEmailPage; 
+		setToken(tokenFromParams)
+
+		const confirmEmail = async () => {
+			try {
+				await verificationService.newVerification(tokenFromParams)
+				toast.success('Email успешно подтвержден!')
+				router.push('/auth/login')
+			} catch (error: any) {
+				toast.error(
+					`Ошибка подтверждения email: ${error.response?.data?.message || 'Неизвестная ошибка'}`
+				)
+				router.push('/auth/register')
+			}
+		}
+
+		confirmEmail()
+	}, [router])
+
+	return (
+		<div>
+			<h1>Подтверждение email...</h1>
+			<p>Пожалуйста, подождите...</p>
+		</div>
+	)
+}
+
+export default ConfirmEmailPage
